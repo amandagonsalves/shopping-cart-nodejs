db575f03869000047242bec18cbc0ce2ab3296ad
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2020-06-03 13:30:16 -0300

    ...

--- index.js
+++ index.js
@@ -3,16 +3,11 @@ const prompt = require('prompt-sync')();
 let nProdutos = prompt('Olá, quantos produtos são? ');
 var ids = []
 let i;
-
-function calc() {
-    for(i=0; i<nProdutos; i++) {
-        var id = Number(prompt('Digite o id do produto: '));
-    }
-    var array = ids.push(id.value);
-    return this.array
+for(i=0; i<nProdutos; i++) {
+    var id = Number(prompt('Digite o id do produto: '))
 }
-calc()
-
+var array = ids.push(id.value);
+console.log(array[id])
 let filterProductsById = products.filter(produto => produto.id);
 //console.log(filterProductsById);
 //if(id!=filterProductsById) {
