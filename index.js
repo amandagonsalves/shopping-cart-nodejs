b315fabe8230d72181f000e07bf58a9d12e9bf83
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2020-06-11 11:51:30 -0300

    function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {products} = require('./data/products.json');
 const prompt = require('prompt-sync')();
 const data = require('./data/products.json');
 
-function returnProducts() {
+function options() {
     console.log(`
    =================================================================
    |    1: Pesquisar produto                                       |
@@ -13,27 +13,61 @@ function returnProducts() {
    |    6: Calcular porcentagem de desconto                        |
    |    7: Parar de comprar                                        |
    =================================================================
-    `)
+`)
+}
+
+function optionSelected(option) {
     let nProducts = []
-    let option = prompt('Digite a opção: ');
+
     if(option=='1') {
-        let searchProduct = prompt('Digite o nome do produto: ').toUpperCase();
-        const filtro = products.filter(searchProduct => { products.name === searchProduct})
-        console.log(filtro)
+
+        //pesquisar produto
+        //let searchProduct = prompt('Digite o nome do produto: ').toUpperCase();
+
     } else if (option == '2') {
-        let addProduct = prompt('Digite o nome do produto: ').toUpperCase();
-        if(addProduct === products.name) {
-            console.log(data)
-            let res = prompt('Adicionar ao carrinho? [Y/N]');
-            if(res == 'Y' || 'y') {
-                nProducts.push(addProduct.value)
-            } else {
-                
-            }
-            
-        }
+
+        //adicionar produto ao carrinho
+        //let addProduct = prompt('Digite o nome do produto: ').toUpperCase();
+        //console.log(addProduct)
+
+    } else if (option == '3') {
+
+        //analisar as promoções
+        console.log(`
+        ======================================================================
+                                PROMOÇÕES DISPONÍVEIS
+        ======================================================================
+            SINGLE LOOK: Todos os produtos do carrinho com a mesma categoria
+            DOUBLE LOOK: Pelo menos 2 produtos de categorias diferentes
+            TRIPLE LOOK: Pelo menos 2 produtos de categorias diferentes
+            FULL LOOK: Pelo menos 4 produtos da mesma categoria
+        ======================================================================
+        `)
+
+    } else if (option == '4') {
+
+        //calcular o valor total com desconto
+
+    } else if (option == '5') {
+
+        //calcular total de desconto
+
+    } else if (option == '6') {
+
+        //Calcular porcentagem de desconto
+
+    } else if (option == '7') {
+
+        console.log('Compra terminada.')
+
     }
     
 }
-
-returnProducts()
+let option;
+while (option != "7") {
+    options();
+    option = prompt('Sua opção: ');
+    const resultado = optionSelected(option);
+    console.log(resultado)
+}
+//returnProducts()
